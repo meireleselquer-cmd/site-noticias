e3cf5508b29e4c351fb7ea9c17984dfa20e2a4a6
DevRafaelaRios <rafaelamaltarios@gmail.com>
2026-04-12 14:18:37 -0300

    ajustes no app

--- app.py
+++ app.py
@@ -449,3 +449,9 @@ if __name__ == "__main__":
         db.create_all()
 
     app.run(host="0.0.0.0", port=5000, debug=True)
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port) 
